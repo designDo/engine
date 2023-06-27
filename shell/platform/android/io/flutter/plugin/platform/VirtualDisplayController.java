60c1618ec9090f4378c393be78a8316404bbf203
tangmaking <tangmaking@163.com>
2023-06-27 14:42:49 +0800

    above Android S, VirtulDisplay use resize method

--- shell/platform/android/io/flutter/plugin/platform/VirtualDisplayController.java
+++ shell/platform/android/io/flutter/plugin/platform/VirtualDisplayController.java
@@ -14,10 +14,15 @@ import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
+import android.os.Looper;
+import android.os.Handler;
+import android.os.Build;
 import android.view.ViewTreeObserver;
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import io.flutter.view.TextureRegistry;
+import io.flutter.Log;
+
 
 @TargetApi(20)
 class VirtualDisplayController {
@@ -55,6 +60,7 @@ class VirtualDisplayController {
         (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
 
     int densityDpi = context.getResources().getDisplayMetrics().densityDpi;
+    Log.e(TAG, "create densityDpi: " + densityDpi);
     VirtualDisplay virtualDisplay =
         displayManager.createVirtualDisplay("flutter-vd", width, height, densityDpi, surface, 0);
 
@@ -84,7 +90,7 @@ class VirtualDisplayController {
   private final int densityDpi;
   private final TextureRegistry.SurfaceTextureEntry textureEntry;
   private final OnFocusChangeListener focusChangeListener;
-  private final Surface surface;
+  private  Surface surface;
 
   private VirtualDisplay virtualDisplay;
   private int bufferWidth;
@@ -127,76 +133,97 @@ class VirtualDisplayController {
   }
 
   public void resize(final int width, final int height, final Runnable onNewSizeFrameAvailable) {
+    final View embeddedView = getView();
+
+    //当 hotreload 触发 resize时，其实大小并没有发生改变，直接 return
+    if(bufferWidth == width && bufferHeight == height) {
+      embeddedView.postDelayed(onNewSizeFrameAvailable, 0);
+      return;
+    }
+
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+       resizeAboveAndroidS(embeddedView, width, height, onNewSizeFrameAvailable);
+       return;
+    }
+    
     boolean isFocused = getView().isFocused();
-    // final SingleViewPresentation.PresentationState presentationState = presentation.detachState();
-    // // We detach the surface to prevent it being destroyed when releasing the vd.
-    // //
-    // // setSurface is only available starting API 20. We could support API 19 by re-creating a new
-    // // SurfaceTexture here. This will require refactoring the TextureRegistry to allow recycling
-    // // texture
-    // // entry IDs.
-    // virtualDisplay.setSurface(null);
-    // virtualDisplay.release();
+    final SingleViewPresentation.PresentationState presentationState = presentation.detachState();
+
+    // We detach the surface to prevent it being destroyed when releasing the vd.
+    //
+    // setSurface is only available starting API 20. We could support API 19 by re-creating a new
+    // SurfaceTexture here. This will require refactoring the TextureRegistry to allow recycling
+    // texture
+    // entry IDs.
+    virtualDisplay.setSurface(null);
+    virtualDisplay.release();
 
     bufferWidth = width;
     bufferHeight = height;
     textureEntry.surfaceTexture().setDefaultBufferSize(width, height);
+    DisplayManager displayManager =
+        (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
+    virtualDisplay =
+        displayManager.createVirtualDisplay("flutter-vd", width, height, densityDpi, surface, 0);
+
+    // There's a bug in Android version older than O where view tree observer onDrawListeners don't
+    // get properly
+    // merged when attaching to window, as a workaround we register the on draw listener after the
+    // view is attached.
+    embeddedView.addOnAttachStateChangeListener(
+        new View.OnAttachStateChangeListener() {
+          @Override
+          public void onViewAttachedToWindow(View v) {
+            OneTimeOnDrawListener.schedule(
+                embeddedView,
+                new Runnable() {
+                  @Override
+                  public void run() {
+                    // We need some delay here until the frame propagates through the vd surface to
+                    // the texture,
+                    // 128ms was picked pretty arbitrarily based on trial and error.
+                    // As long as we invoke the runnable after a new frame is available we avoid the
+                    // scaling jank
+                    // described in: https://github.com/flutter/flutter/issues/19572
+                    // We should ideally run onNewSizeFrameAvailable ASAP to make the embedded view
+                    // more responsive
+                    // following a resize.
+                    Log.e(TAG, "onNewSizeFrameAvailable::");
+                    embeddedView.postDelayed(onNewSizeFrameAvailable, 128);
+                  }
+                });
+            embeddedView.removeOnAttachStateChangeListener(this);
+          }
+
+          @Override
+          public void onViewDetachedFromWindow(View v) {}
+        });
+
+    // Create a new SingleViewPresentation and show() it before we cancel() the existing
+    // presentation. Calling show() and cancel() in this order fixes
+    // https://github.com/flutter/flutter/issues/26345 and maintains seamless transition
+    // of the contents of the presentation.
+      SingleViewPresentation newPresentation =
+      new SingleViewPresentation(
+          context,
+          virtualDisplay.getDisplay(),
+          accessibilityEventsDelegate,
+          presentationState,
+          focusChangeListener,
+          isFocused);
+  newPresentation.show();
+  presentation.cancel();
+  presentation = newPresentation;
+  }
+
+  //Android 31 以上，Presentation 的实现 在全屏旋转时会导致 BufferQueue has no connected producer。
+  //推测是 VirtualDisplay 重新创建导致的 之前的 Surface 被回收了。在这里直接调用 resize 方法，不重新创建。
+  private void resizeAboveAndroidS(View embeddedView, int width, int height, final Runnable onNewSizeFrameAvailable) {
+    bufferWidth = width;
+    bufferHeight = height;
+    textureEntry.surfaceTexture().setDefaultBufferSize(width, height);
     virtualDisplay.resize(width, height, densityDpi);
-    final View embeddedView = getView();
-    embeddedView.postDelayed(onNewSizeFrameAvailable, 128);
-    // DisplayManager displayManager =
-    //     (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
-    // virtualDisplay =
-    //     displayManager.createVirtualDisplay("flutter-vd", width, height, densityDpi, surface, 0);
-
-    // final View embeddedView = getView();
-    // // There's a bug in Android version older than O where view tree observer onDrawListeners don't
-    // // get properly
-    // // merged when attaching to window, as a workaround we register the on draw listener after the
-    // // view is attached.
-    // embeddedView.addOnAttachStateChangeListener(
-    //     new View.OnAttachStateChangeListener() {
-    //       @Override
-    //       public void onViewAttachedToWindow(View v) {
-    //         OneTimeOnDrawListener.schedule(
-    //             embeddedView,
-    //             new Runnable() {
-    //               @Override
-    //               public void run() {
-    //                 // We need some delay here until the frame propagates through the vd surface to
-    //                 // the texture,
-    //                 // 128ms was picked pretty arbitrarily based on trial and error.
-    //                 // As long as we invoke the runnable after a new frame is available we avoid the
-    //                 // scaling jank
-    //                 // described in: https://github.com/flutter/flutter/issues/19572
-    //                 // We should ideally run onNewSizeFrameAvailable ASAP to make the embedded view
-    //                 // more responsive
-    //                 // following a resize.
-    //                 embeddedView.postDelayed(onNewSizeFrameAvailable, 128);
-    //               }
-    //             });
-    //         embeddedView.removeOnAttachStateChangeListener(this);
-    //       }
-
-    //       @Override
-    //       public void onViewDetachedFromWindow(View v) {}
-    //     });
-
-    // // Create a new SingleViewPresentation and show() it before we cancel() the existing
-    // // presentation. Calling show() and cancel() in this order fixes
-    // // https://github.com/flutter/flutter/issues/26345 and maintains seamless transition
-    // // of the contents of the presentation.
-    // SingleViewPresentation newPresentation =
-    //     new SingleViewPresentation(
-    //         context,
-    //         virtualDisplay.getDisplay(),
-    //         accessibilityEventsDelegate,
-    //         presentationState,
-    //         focusChangeListener,
-    //         isFocused);
-    // newPresentation.show();
-    // presentation.cancel();
-    // presentation = newPresentation;
+    embeddedView.postDelayed(onNewSizeFrameAvailable, 0);
   }
 
   public void dispose() {
@@ -268,6 +295,7 @@ class VirtualDisplayController {
       if (mOnDrawRunnable == null) {
         return;
       }
+
       mOnDrawRunnable.run();
       mOnDrawRunnable = null;
       mView.post(
